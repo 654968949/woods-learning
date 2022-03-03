685657b628202dcb780f3014ab381a59c464ac67
Woods <woodswu@candao.com>
2022-03-03 10:46:46 +0800

    责任链模式和建造者模式的结合

---
+++ design-pattern/src/main/java/behavior/chain/ValidateHandler.java
@@ -0,0 +1,22 @@
+package behavior.chain;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * @author : Woods
+ * @ClassName : ValidateHandler
+ * @date : 2022/2/28  9:27 上午
+ * @Version ：1.0
+ */
+public class ValidateHandler extends Handler {
+
+    @Override
+    public void doHandler(Member member) {
+        if(StrUtil.isBlank(member.getLoginName()) || StrUtil.isEmpty(member.getloginPass())){
+            System.out.println("用户名或密码为空");
+            return;
+        }
+        System.out.println("用户名密码校验成功,可以往下执行");
+        chain.doHandler(member);
+    }
+}
