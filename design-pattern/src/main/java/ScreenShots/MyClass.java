b80c042c34a8650319d92cb953acf8ba5ea7b7b4
Woods <woodswu@candao.com>
2022-07-22 16:52:52 +0800

    txt

---
+++ design-pattern/src/main/java/ScreenShots/MyClass.java
@@ -0,0 +1,19 @@
+package ScreenShots;
+
+/**
+ * @author : Woods
+ * @ClassName : MyClass
+ * @date : 2022/7/22  4:52 下午
+ * @Version ：1.0
+ */
+public class MyClass {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
